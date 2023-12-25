ba619da69d952bc2ee1deb72126f467719266db4
Bhushan Yadav <30519282+bhushanyadav07@users.noreply.github.com>
2023-12-25 22:19:13 +0530

    Update Valid_Anagram.js
    my code for anagram

--- Valid_Anagram.js
+++ Valid_Anagram.js
@@ -1,1 +1,15 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    const isEqual = s.length === t.length;
+    if(!isEqual) {return false}
+    return helperFunction(s) === helperFunction(t);
+};
 
+const helperFunction = (str) => str
+    .split('')
+    .sort((a,b) => a.localeCompare(b))
+    .join('');
